46f2bf0f4b11629c7ab205c0c6a19398558d0e84
PabloOteroDeMiguel <potero91@gmail.com>
2020-11-06 18:23:56 +0100

    Intento de creacion de post

--- Posts/views.py
+++ Posts/views.py
@@ -7,3 +7,13 @@ def home(request):
      latest_posts = Post.objects.all().order_by("-created_at")
      context = {'Posts': latest_posts}
      return render(request, "home.html", context)
+
+def post_detail(request, pk):
+    possible_post = Post.objects.filter(pk=pk).select_related("category")
+    if len(possible_post) == 0:
+        # pelicula no existe
+        return render(request, "404.html", status=404)
+    else:
+        post = possible_post[0]
+        context = {'post': Post}
+        return render(request, "post_detail.html", context)
